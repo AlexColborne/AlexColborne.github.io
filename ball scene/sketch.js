ea06dd48154f6b525a6b9304eb2772edfa6d3aad
AlexColborne <90922634+AlexColborne@users.noreply.github.com>
2021-09-21 09:44:10 -0600

    Perlin Noise Demo

--- ball scene/sketch.js
+++ ball scene/sketch.js
@@ -6,9 +6,9 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // for(let index = 0; index < 30; index++) {
-  //   spawnBall(random(width), random(height));
-  // }
+  for(let index = 0; index < 30; index++) {
+    spawnBall();
+  }
 
   //spawn a ball every 0.5 seconds
   window.setInterval(spawnBall, 500);
@@ -21,27 +21,36 @@ function draw() {
 }
 
 function mouseClicked() {
-  spawnBall(mouseX, mouseY);
+  spawnBall();
+  ballArray[ballArray.length - 1].x = mouseX;
+  ballArray[ballArray.length - 1].y = mouseY;
 }
 
-function spawnBall(xSpawn, ySpawn) {
+function spawnBall() {
   let newBall = {
-    x: xSpawn,
-    y: ySpawn,
+    x: random(width),
+    y: random(height),
     diameter: random(20, 60),
     ballColor: color(random(255), random(255), random(255), random(255)),
     dx: random(5, 10),
     dy: random(5, 10),
+    xTime: random(1000),
+    yTime: random(1000),
+    timeChange: random(0.001, 0.01),
   };
   ballArray.push(newBall);
 }
 
 function moveBall() {
   for (let theBall of ballArray) {
-    theBall.x += theBall.dx;
-    theBall.y += theBall.dy;
-    theBall.dx = random(-5, 5);
-    theBall.dy = random(-5, 5);
+    // theBall.x += theBall.dx;
+    // theBall.y += theBall.dy;
+    // theBall.dx = random(-5, 5);
+    // theBall.dy = random(-5, 5);
+    theBall.x = noise(theBall.xTime) * width;
+    theBall.y = noise(theBall.yTime) * height;
+    theBall.xTime += theBall.timeChange;
+    theBall.yTime += theBall.timeChange;
   }
 }
 
