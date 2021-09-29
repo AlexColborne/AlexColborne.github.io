cd5ae5138eb72b3385d229c77908c41fc248b7f5
AlexColborne <90922634+AlexColborne@users.noreply.github.com>
2021-09-29 10:18:56 -0600

    created tetris grid

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -11,12 +11,29 @@ let cellSize;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = createGrid(20, 10);
+  cellSize = height/20;
 }
 
 function draw() {
   background(220);
+  drawGrid();
 }
 
-function createGrid(x, y) {
+function createGrid(gridHeight, gridWidth) {
+  let gridArray = [];
+  for(let y = 0; y < gridHeight; y++) {
+    gridArray.push([]);
+    for(let x = 0; x<gridWidth; x++) {
+      gridArray[y].push(0);
+    }
+  }
+  return gridArray;
+}
 
+function drawGrid() {
+  for(let y = 0; y < 20; y++) {
+    for(let x = 0; x < 10; x++) {
+      rect(x*cellSize + width/2 - cellSize * 5, y*cellSize, cellSize);
+    }
+  }
 }
